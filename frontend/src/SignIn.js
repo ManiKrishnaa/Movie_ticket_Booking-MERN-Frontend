900d4591ff109d3a10dda78bd88557b875bec518
ManiKrishnaa <130267764+ManiKrishnaa@users.noreply.github.com>
2024-06-03 13:32:25 +0530

    Add files via upload

---
+++ frontend/src/SignIn.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+function SignIn({handleLogin}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        try {
+            const response = await fetch('http://localhost:5000/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            const data = await response.json();
+
+            if (data.status == 'ok') {
+                localStorage.setItem('isloggedin',true);
+                localStorage.setItem('userdata',JSON.stringify(data.userdata));
+                handleLogin();
+                navigate('/movies');
+            } else if(data.status == 'nouser') {
+                alert('no user found ! please signup first !');
+                return;
+            } else if(data.status == 'invalid_credintials'){
+                alert('Invalid Credintials !');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred, please try again later');
+        }
+    };
+
+
+    return (
+        <div className="signin">
+            <h2>Sign In</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Your Email" required />
+                </div>
+                <div className="form-group">
+                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Your Password" required />
+                </div>
+                <button type="submit">Sign In</button>
+            </form>
+            <p><Link to='/signup' className="signin-title">Sign up here</Link></p>
+        </div>
+    );
+}
+
+export default SignIn;
