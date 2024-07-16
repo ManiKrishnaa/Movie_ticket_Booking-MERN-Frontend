3a94c2652559eff5247e3c4e16bf166145080c71
ManiKrishnaa <130267764+ManiKrishnaa@users.noreply.github.com>
2024-07-16 20:53:51 +0530

    Update MovieDetails.js

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -14,7 +14,7 @@ function MovieDetails() {
     const fetchMovieDetails = async () => {
       console.log(`Fetching movie details for id: ${id}`);
       try {
-        const response = await axios.get(`http://localhost:5000/moviess/${id}`);
+        const response = await axios.get(`https://movie-ticket-booking-mern-backend-d4l9-4n3hfsha6.vercel.app/moviess/${id}`);
         console.log('Response data:', response.data);
         if (response.data.movie) {
           setMovieDetails(response.data.movie);
@@ -56,7 +56,7 @@ function MovieDetails() {
     try {
       const encodedTheater = encodeURIComponent(theater.trim());
       const encodedDate = encodeURIComponent(date.trim());
-      const response = await axios.get(`http://localhost:5000/movies/${id}/${encodedTheater}/${showtime}/${encodedDate}`);
+      const response = await axios.get(`https://movie-ticket-booking-mern-backend-d4l9-4n3hfsha6.vercel.app/movies/${id}/${encodedTheater}/${showtime}/${encodedDate}`);
       const data = response.data.seatingData;
       localStorage.setItem('seatings', JSON.stringify(data));
       window.location.reload();
